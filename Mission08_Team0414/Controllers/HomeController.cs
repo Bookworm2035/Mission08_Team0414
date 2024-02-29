af67d00faff4ad4b9dd4c9b8c4bddd9147988677
liliantsubaki <liliantsubaki@gmail.com>
2024-02-28 17:14:17 -0700

    Commented out the homecontroller stuff to create a migration Created a database!! (Note, this push does not include me added the EF and I repository files back in)

--- Mission08_Team0414/Controllers/HomeController.cs
+++ Mission08_Team0414/Controllers/HomeController.cs
@@ -6,14 +6,14 @@ namespace Mission08_Team0414.Controllers
 {
     public class HomeController : Controller
     {
-        private TaskContext _TaskContext;
-        //This is whatever the database is named?
-        private ITaskRepository _repo;
+        //private TaskContext _TaskContext;
+        ////This is whatever the database is named?
+        //private ITaskRepository _repo;
 
-        public HomeController(ITaskRepository temp)
-        {
-            _repo = temp;
-        }
+        //public HomeController(ITaskRepository temp)
+        //{
+        //    _repo = temp;
+        //}
 
         public HomeController(TaskContext TaskContext)
         {
@@ -26,91 +26,91 @@ namespace Mission08_Team0414.Controllers
             return View();
         }
 
-        //View that displays allows you to add tasks?
-        [HttpGet]
-        public IActionResult Tasks()
-        {
-            ViewBag.Categories = _TaskContext.Categories
-               .OrderBy(x => x.CategoryName)
-               .ToList();
-            return base.View("Tasks", new System.Threading.Tasks.Task());
-        }
+        ////View that displays allows you to add tasks?
+        //[HttpGet]
+        //public IActionResult Tasks()
+        //{
+        //    ViewBag.Categories = _TaskContext.Categories
+        //       .OrderBy(x => x.CategoryName)
+        //       .ToList();
+        //    return base.View("Tasks", new System.Threading.Tasks.Task());
+        //}
 
-        [HttpPost]
-        public IActionResult Tasks(System.Threading.Tasks.Task response)
-        {
-            if (ModelState.IsValid)
-            {
-                //confirm that the submission meets requirements
-                _TaskContext.Movies.Add(response);
-                _TaskContext.SaveChanges();
-                return View("Confirmation", response);
+        //[HttpPost]
+        //public IActionResult Tasks(System.Threading.Tasks.Task response)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        //confirm that the submission meets requirements
+        //        _TaskContext.Movies.Add(response);
+        //        _TaskContext.SaveChanges();
+        //        return View("Confirmation", response);
 
-            }
-            else
-            {
-                //if bad return error meessages
-                ViewBag.Categories = _TaskContext.Categories
-                    .OrderBy(x => x.CategoryName)
-                    .ToList();
-                return View(response);
-            }
-        }
-        //View that displays all the quadrants with tasks?
-        public IActionResult Quadrants()
-        {
-            //display database
-            var tasks = _TaskContext.Tasks.Include(m => m.Category)
-                         //.Where(x => x.COLUM == value)
-                         .OrderBy(x => x.TaskId).ToList();
-            return View(tasks);
-        }
-        //delete a task
-        [HttpGet]
-        public IActionResult Delete(int id)
-        {
-            //delete the record by ID num
-            var recordToDelete = _TaskContext.Tasks
-                .Single(x => x.TaskId == id);
+        //    }
+        //    else
+        //    {
+        //        //if bad return error meessages
+        //        ViewBag.Categories = _TaskContext.Categories
+        //            .OrderBy(x => x.CategoryName)
+        //            .ToList();
+        //        return View(response);
+        //    }
+        //}
+        ////View that displays all the quadrants with tasks?
+        //public IActionResult Quadrants()
+        //{
+        //    //display database
+        //    var tasks = _TaskContext.Tasks.Include(m => m.Category)
+        //                 //.Where(x => x.COLUM == value)
+        //                 .OrderBy(x => x.TaskId).ToList();
+        //    return View(tasks);
+        //}
+        ////delete a task
+        //[HttpGet]
+        //public IActionResult Delete(int id)
+        //{
+        //    //delete the record by ID num
+        //    var recordToDelete = _TaskContext.Tasks
+        //        .Single(x => x.TaskId == id);
 
-            return View(recordToDelete);
-        }
-        [HttpPost]
-        public IActionResult Delete(System.Threading.Tasks.Task task)
-        {
-            //actually delete it
-            _TaskContext.Movies.Remove(task);
-            _TaskContext.SaveChanges();
+        //    return View(recordToDelete);
+        //}
+        //[HttpPost]
+        //public IActionResult Delete(System.Threading.Tasks.Task task)
+        //{
+        //    //actually delete it
+        //    _TaskContext.Movies.Remove(task);
+        //    _TaskContext.SaveChanges();
 
-            return RedirectToAction("Quadrant");
-        }
-        //edit a task
-        [HttpGet]
-        public IActionResult Edit(int id)
+        //    return RedirectToAction("Quadrant");
+        //}
+        ////edit a task
+        //[HttpGet]
+        //public IActionResult Edit(int id)
 
-        {
-            var recordToEdit = _TaskContext.Task
-                .Single(x => x.TaskId == id);
+        //{
+        //    var recordToEdit = _TaskContext.Task
+        //        .Single(x => x.TaskId == id);
           
 
-            ViewBag.Categories = _TaskContext.Categories
-                .OrderBy(x => x.CategoryName)
-                .ToList();
-            return View("Tasks", recordToEdit);
-        }
-        [HttpPost]
-        public IActionResult Edit(System.Threading.Tasks.Task updateresponse)
-        {
-            //update the datebase with the new edits
-            _TaskContext.Update(updateresponse);
-            _TaskContext.SaveChanges();
-            //return to view
-            return RedirectToAction("Quadrant", "Home");
-        }
-        public IActionResult Add()
-        {
-            return View("Tasks");
-        }
+        //    ViewBag.Categories = _TaskContext.Categories
+        //        .OrderBy(x => x.CategoryName)
+        //        .ToList();
+        //    return View("Tasks", recordToEdit);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(System.Threading.Tasks.Task updateresponse)
+        //{
+        //    //update the datebase with the new edits
+        //    _TaskContext.Update(updateresponse);
+        //    _TaskContext.SaveChanges();
+        //    //return to view
+        //    return RedirectToAction("Quadrant", "Home");
+        //}
+        //public IActionResult Add()
+        //{
+        //    return View("Tasks");
+        //}
 
     }
 }
--- Mission08_Team0414/Models/ITaskRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mission08_Team0414.Models
-{
-    public interface ITaskRepository
-    {
-        List<System.Threading.Tasks.Task> Tasks { get; }
-
-        public void AddTask(System.Threading.Tasks.Task task);
-    }
-}
